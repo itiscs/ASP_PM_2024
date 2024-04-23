6dbfd9a3e9527b3d23a41240a31f80e7ff9504e6
itiscs <moudroff@gmail.com>
2024-04-23 17:50:36 +0300

    Identity

---
+++ IdentityApp/IdentityApp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Models
+{
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Наименование")]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string? Description { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Цена от 0 до 1000")]
+        [Display(Name = "Цена")]
+        public decimal Price { get; set; }
+    
+        [Display(Name = "Количество")]
+        public int Count { get; set; }
+    }
+}
